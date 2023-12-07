e0fbe983831f6d4406c105e0e76019f42e64179d
sasafokin2@gmail.com <PC@JODIFO-LAPTOP>
2023-12-07 22:55:54 +0300

    Finished lesson 5

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Categories, SortPopup, PizzaBlock } from "../components";
+
+function Home({ pizzas }) {
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories
+          items={["Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"]}
+        />
+        <SortPopup items={["популярности", "цене", "алфавиту"]} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {pizzas &&
+          pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
